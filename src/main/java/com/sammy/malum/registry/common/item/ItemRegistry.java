a6a4ad019fb50f0eccc8378d6530020e9c606b54
AtobaAzul <atoba.azul.radical@gmail.com>
2025-02-20 18:11:30 -0300

    fix + data run
    Crashes but oh well

--- src/main/java/com/sammy/malum/registry/common/item/ItemRegistry.java
+++ src/main/java/com/sammy/malum/registry/common/item/ItemRegistry.java
@@ -576,7 +576,7 @@ public class ItemRegistry {
     public static final DeferredHolder<Item, Item> FRACTURED_TIN_IMPETUS = register("fractured_tin_impetus", METALLURGIC_PROPERTIES(), CrackedImpetusItem::new);
     public static final DeferredHolder<Item, Item> TIN_NODE = register("tin_node", METALLURGIC_NODE_PROPERTIES(), NodeItem::new);
     public static final DeferredHolder<Item, CrackedImpetusItem> CRACKED_COBALT_IMPETUS = register("cracked_cobalt_impetus", METALLURGIC_PROPERTIES(), CrackedImpetusItem::new);
-    public static final DeferredHolder<Item, ImpetusItem> COBALT_IMPETUS = register("cobalt_impetus", METALLURGIC_PROPERTIES().durability(800), (p) -> new ImpetusItem(p).setCrackedVariant(CRACKED_COBALT_IMPETUS));
+    public static final DeferredHolder<Item, ImpetusItem> COBALT_IMPETUS = register("cobalt_impetus", METALLURGIC_PROPERTIES(), ImpetusItem::new);
     public static final DeferredHolder<Item, Item> COBALT_NODE = register("cobalt_node", METALLURGIC_NODE_PROPERTIES(), NodeItem::new);
 
     public static final DeferredHolder<Item, Item> ZINC_IMPETUS = register("zinc_impetus", METALLURGIC_PROPERTIES(), ImpetusItem::new);
