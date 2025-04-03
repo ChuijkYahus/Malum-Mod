166d6ec86527f54f447964fbc8c893746b07a489
Biverom <biverom@gmail.com>
2025-04-03 22:19:38 +0200

    getter

--- src/main/java/com/sammy/malum/mixin/client/CreativeModeInventoryScreenMixin.java
+++ src/main/java/com/sammy/malum/mixin/client/CreativeModeInventoryScreenMixin.java
@@ -2,7 +2,6 @@ package com.sammy.malum.mixin.client;
 
 import com.sammy.malum.core.handlers.hiding.HiddenTagHandler;
 import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
-import net.minecraft.world.inventory.AbstractContainerMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,9 +12,7 @@ public class CreativeModeInventoryScreenMixin {
 
     @Inject(method = "refreshSearchResults", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/NonNullList;addAll(Ljava/util/Collection;)Z", shift = At.Shift.AFTER))
     private void malum$refreshSearchResults(CallbackInfo ci) {
-        AbstractContainerScreenAccessor<?> accessor = (AbstractContainerScreenAccessor<?>) (Object) this;
-        AbstractContainerMenu menu = accessor.malum$getMenu();
-        var itemPickerMenu = ((CreativeModeInventoryScreen.ItemPickerMenu)menu);
-        HiddenTagHandler.hideItems(itemPickerMenu.items);
+        CreativeModeInventoryScreen.ItemPickerMenu menu = ((CreativeModeInventoryScreen) (Object) this).getMenu();
+        HiddenTagHandler.hideItems(menu.items);
     }
 }
