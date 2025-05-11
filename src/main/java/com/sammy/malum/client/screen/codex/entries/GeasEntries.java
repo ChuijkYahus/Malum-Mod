b8452c02e360be47475e9ee92c06763d1a6b5da6
SammySemicolon <malummoddevelopment@gmail.com>
2025-05-11 21:11:25 +0200

    Waa

--- src/main/java/com/sammy/malum/client/screen/codex/entries/GeasEntries.java
+++ src/main/java/com/sammy/malum/client/screen/codex/entries/GeasEntries.java
@@ -61,23 +61,17 @@ public class GeasEntries {
         addGeasEntry(screen, MalumGeasEffectTypeRegistry.INFERNAL_1, -4, 13);
         addGeasEntry(screen, MalumGeasEffectTypeRegistry.INFERNAL_2, -5, 14);
 
-//        addGeasEntry(screen, MalumGeasEffectTypeRegistry.SACRED_3, 18, 13);
-//        addGeasEntry(screen, MalumGeasEffectTypeRegistry.SACRED_4, 19, 11);
-
-        addGeasEntry(screen, MalumGeasEffectTypeRegistry.PACT_OF_THE_REAPER, 19, 14);
-//        addGeasEntry(screen, MalumGeasEffectTypeRegistry.WICKED_4, 20, 12);
-
-        addGeasEntry(screen, MalumGeasEffectTypeRegistry.PACT_OF_RECIPROCATION, -18, 13);
-//        addGeasEntry(screen, MalumGeasEffectTypeRegistry.ARCANE_4, -19, 11);
-
-//        addGeasEntry(screen, MalumGeasEffectTypeRegistry.ELDRITCH_3, -19, 14);
-//        addGeasEntry(screen, MalumGeasEffectTypeRegistry.ELDRITCH_4, -20, 12);
+        addGeasEntry(screen, MalumGeasEffectTypeRegistry.PACT_OF_THE_LIFEWEAVER, 15, 14);
+        addGeasEntry(screen, MalumGeasEffectTypeRegistry.PACT_OF_THE_BERSERKER, 14, 15);
 
         addGeasEntry(screen, MalumGeasEffectTypeRegistry.PACT_OF_THE_SKYBREAKER, 13, 17);
         addGeasEntry(screen, MalumGeasEffectTypeRegistry.PACT_OF_THE_CLOUDSKIPPER, 15, 16);
 
         addGeasEntry(screen, MalumGeasEffectTypeRegistry.AQUEOUS_3, 14, 18);
         addGeasEntry(screen, MalumGeasEffectTypeRegistry.AQUEOUS_4, 16, 17);
+
+        addGeasEntry(screen, MalumGeasEffectTypeRegistry.PACT_OF_RECIPROCATION, -15, 14);
+        addGeasEntry(screen, MalumGeasEffectTypeRegistry.PACT_OF_THE_RUNE_EXPLOITATION, -14, 15);
 
         addGeasEntry(screen, MalumGeasEffectTypeRegistry.PACT_OF_THE_PROFANE_ASCETIC, -13, 17);
         addGeasEntry(screen, MalumGeasEffectTypeRegistry.PACT_OF_THE_PROFANE_GLUTTON, -15, 16);
---
+++ src/main/java/com/sammy/malum/common/geas/pact/wicked/BerserkerGeas.java
@@ -0,0 +1,47 @@
+package com.sammy.malum.common.geas.pact.wicked;
+
+import com.sammy.malum.common.entity.scythe.ScytheBoomerangEntity;
+import com.sammy.malum.common.item.curiosities.weapons.scythe.MalumScytheItem;
+import com.sammy.malum.common.worldevent.DelayedDamageWorldEvent;
+import com.sammy.malum.core.handlers.SoulDataHandler;
+import com.sammy.malum.core.helpers.ComponentHelper;
+import com.sammy.malum.core.systems.geas.GeasEffect;
+import com.sammy.malum.registry.common.DamageTypeRegistry;
+import com.sammy.malum.registry.common.MalumGeasEffectTypeRegistry;
+import com.sammy.malum.registry.common.ParticleEffectTypeRegistry;
+import com.sammy.malum.registry.common.SoundRegistry;
+import com.sammy.malum.registry.common.item.EnchantmentRegistry;
+import com.sammy.malum.registry.common.tag.DamageTypeTagRegistry;
+import net.minecraft.core.Holder;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.util.Mth;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+import team.lodestar.lodestone.handlers.WorldEventHandler;
+import team.lodestar.lodestone.helpers.DamageTypeHelper;
+import team.lodestar.lodestone.registry.common.LodestoneAttributes;
+
+import java.util.function.Consumer;
+
+import static net.minecraft.world.entity.EquipmentSlot.MAINHAND;
+
+public class BerserkerGeas extends GeasEffect {
+
+    public BerserkerGeas() {
+        super(MalumGeasEffectTypeRegistry.PACT_OF_THE_REAPER.get());
+    }
+
+    @Override
+    public void addTooltipComponents(LivingEntity entity, Consumer<Component> tooltipAcceptor, TooltipFlag tooltipFlag) {
+        tooltipAcceptor.accept(ComponentHelper.positiveGeasEffect("scythe_combo"));
+        tooltipAcceptor.accept(ComponentHelper.negativeGeasEffect("only_scythe"));
+        super.addTooltipComponents(entity, tooltipAcceptor, tooltipFlag);
+    }
+}
