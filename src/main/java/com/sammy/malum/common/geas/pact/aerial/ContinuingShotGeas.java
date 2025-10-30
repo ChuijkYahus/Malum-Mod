9ef774ced3a2580cb2b38d34058dd2b7bafe5c46
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2025-10-30 18:28:58 +0100

    Continuing Shot Effect Textures

--- src/main/java/com/sammy/malum/common/geas/pact/aerial/ContinuingShotGeas.java
+++ src/main/java/com/sammy/malum/common/geas/pact/aerial/ContinuingShotGeas.java
@@ -33,7 +33,7 @@ public class ContinuingShotGeas extends GeasEffect {
     @Override
     public void finalizedOutgoingDamageEvent(LivingDamageEvent.Post event, LivingEntity attacker, LivingEntity target, ItemStack stack) {
         final Entity projectile = event.getSource().getDirectEntity();
-        if (projectile instanceof AbstractArrow || projectile instanceof AbstractBoltProjectileEntity) {
+        if ((projectile instanceof AbstractArrow arrow && arrow.isCritArrow()) || projectile instanceof AbstractBoltProjectileEntity) {
             applyEffect(attacker, true);
         }
     }
