9f9e5f7dad987e03158cb3ade8cfdf74009612d6
SammySemicolon <malummoddevelopment@gmail.com>
2026-07-04 20:00:51 +0200

    Test

--- src/main/java/com/sammy/malum/client/renderer/entity/cultist/altar/AltarRenderer.java
+++ src/main/java/com/sammy/malum/client/renderer/entity/cultist/altar/AltarRenderer.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.sammy.malum.MalumMod;
 import com.sammy.malum.client.model.mob.altar.AltarModel;
 import com.sammy.malum.common.entity.mob.cultist.altar.AltarCultist;
+import earth.terrarium.athena.api.client.utils.AthenaUtils;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
@@ -14,7 +15,7 @@ import org.jetbrains.annotations.NotNull;
 @OnlyIn(Dist.CLIENT)
 public class AltarRenderer extends MobRenderer<AltarCultist, AltarModel> {
     private static final ResourceLocation ALTAR_LOCATION = MalumMod.malumPath("textures/entity/cultist/altar.png");
-
+    AthenaUtils
     public AltarRenderer(EntityRendererProvider.Context context) {
         super(context, new AltarModel(context.bakeLayer(AltarModel.LAYER)), 0.4F);
     }
