70caf9ae621e7990724b672b3673103da294bfd6
Uraneptus <50745606+Uraneptus@users.noreply.github.com>
2026-07-08 20:49:15 +0200

    Rarities, Colors and datagen???
    why does datagen always regenerate all files on a new device

--- src/main/java/com/sammy/malum/registry/common/item/MalumItemProperties.java
+++ src/main/java/com/sammy/malum/registry/common/item/MalumItemProperties.java
@@ -1,6 +1,7 @@
 package com.sammy.malum.registry.common.item;
 
 import com.sammy.malum.registry.common.*;
+import net.minecraft.world.item.Rarity;
 import team.lodestar.lodestone.modules.toolkit.item.*;
 
 public class MalumItemProperties {
@@ -15,5 +16,9 @@ public class MalumItemProperties {
 
     public static LodestoneItemProperties IMPETUS() {
         return DEFAULT().stacksTo(1);
+    }
+
+    public static LodestoneItemProperties SPIRITS(Rarity rarity) {
+        return DEFAULT().rarity(rarity);
     }
 }
